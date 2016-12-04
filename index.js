202ebd178da0beb7259eb731a00a4575d5dd1fdd
Abhinav <abhinavhavaldar@hotmail.com>
2016-12-04 17:14:07 -0500

    change cron recur time

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var app         = express();
 var trello      = new Trello(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
 var rule        = new chron.RecurrenceRule();
 rule.dayOfWeek  = [0, new chron.Range(0, 6)];
-rule.hour       = 11;
-rule.minute     = 0;
+rule.hour       = 17;
+rule.minute     = 15;
 
 var archiveTask = chron.scheduleJob(rule, function() {
     t.post('/1/lists/' + process.env.LIST_ID + '/archiveAllCards', function(err, data) { console.log(data); });
