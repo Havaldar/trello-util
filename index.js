6f807951602f9fc21658afad473e94216e291e11
Abhinav <abhinavhavaldar@hotmail.com>
2016-12-04 17:39:40 -0500

    haha

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var rule        = new chron.RecurrenceRule();
 rule.dayOfWeek  = [0, new chron.Range(0, 6)];
 rule.hour       = 17;
 rule.minute     = 15;
-t.post('/1/lists/' + process.env.LIST_ID + '/archiveAllCards', function(err, data) { console.log(data); });
+trello.post('/1/lists/' + process.env.LIST_ID + '/archiveAllCards', function(err, data) { console.log(data); });
 
